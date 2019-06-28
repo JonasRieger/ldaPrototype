9405d2fa9d024b271da3affc3af4d9953d7b3930
Jonas Rieger <jonas.rieger@tu-dortmund.de>
2019-06-28 11:11:59 +0200

    typo

--- R/jaccardTopics.R
+++ R/jaccardTopics.R
@@ -15,7 +15,7 @@
 #' @param limit.abs [\code{integer(1)}]\cr
 #' An absolute lower bound limit for which words are taken into account. All words
 #' are taken as relevant for a topic that have a count higher than \code{limit.abs}.
-#' Default is \cdoe{10}.
+#' Default is \code{10}.
 #' @param progress [\code{logical(1)}]\cr
 #' Should a nice progress bar be shown? Turning it off, could lead to significantly
 #' faster calculation. Default ist \code{TRUE}.
