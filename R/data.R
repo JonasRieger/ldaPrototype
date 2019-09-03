dde42bf138fa0add62a284059cd59f9e5c0e155a
Jonas Rieger <jonas.rieger@tu-dortmund.de>
2019-09-03 12:14:46 +0200

    new data

--- R/data.R
+++ R/data.R
@@ -6,6 +6,11 @@
 #' @aliases reuters_docs reuters_vocab docs vocab
 #'
 #' @docType data
+#' @format
+#' \code{reuters_docs} is a list of documents of length 91 prepared by \code{\link[tosca]{LDAprep}}.
+#'
+#' \code{reuters_vocab} is a \code{character} vector of length 2139
+#'
 #' @usage data(reuters_docs)
 #'
 #' @source
@@ -14,6 +19,7 @@
 #' @references
 #' Lewis, David (1997). \emph{Reuters-21578 Text Categorization Collection Distribution 1.0}.
 #' \url{http://kdd.ics.uci.edu/databases/reuters21578/reuters21578.html}
+#'
 #' Luz, Saturnino. \emph{XML-encoded version of Reuters-21578}.
 #' \url{http://ronaldo.cs.tcd.ie/esslli07/data/reuters21578-xml/}
 "reuters_docs"
