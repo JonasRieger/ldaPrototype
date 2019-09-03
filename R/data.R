7b5ed122607e91d51e029ab1d81b0ffb744c3d07
Jonas Rieger <jonas.rieger@tu-dortmund.de>
2019-09-03 12:01:54 +0200

    reuters data

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' @title A Snippet of the Reuters Dataset
+#'
+#' @description bla
+#'
+#' @name reuters
+#' @aliases reuters_docs reuters_vocab docs vocab
+#'
+#' @docType data
+#' @usage data(reuters_docs)
+#'
+#' @source
+#' \url{http://ronaldo.cs.tcd.ie/esslli07/data/reuters21578-xml/}
+#'
+#' @references
+#' Lewis, David (1997). \emph{Reuters-21578 Text Categorization Collection Distribution 1.0}.
+#' \url{http://kdd.ics.uci.edu/databases/reuters21578/reuters21578.html}
+#' Luz, Saturnino. \emph{XML-encoded version of Reuters-21578}.
+#' \url{http://ronaldo.cs.tcd.ie/esslli07/data/reuters21578-xml/}
+"reuters_docs"
+
+#' @rdname reuters
+#' @usage data(reuters_vocab)
+"reuters_vocab"
